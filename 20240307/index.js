5d485f4c0fbde66cf1948aa2017e8c4c3e8571da
yohanaff <yohana@yotec.eti.br>
2024-03-12 12:32:58 -0300

    Exercicios 01 a 03

---
+++ 20240307/index.js
@@ -0,0 +1,5 @@
+let elemento = document.getElementById('elementoClique');
+
+elemento.addEventListener('click', function () {
+    elemento.style.cssText = 'background-color: red; height: 200px; width: 400px; display: flex; justify-content: center; align-items: center;'
+});
